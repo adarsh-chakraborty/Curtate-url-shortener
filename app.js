f4d17af2a66e7fc3f953ecb59a8eafe694dde9f3
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-06-22 19:51:29 +0530

    Support for all favicons

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require("express");
-
+const path = require("path");
 const mainRoutes = require("./routes/routes");
 const sequelize = require('./util/database');
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(mainRoutes);
